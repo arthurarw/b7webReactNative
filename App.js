ed0e5a18ba1c58a2c6d1e3f872c8ff2349efa356
Arthur Warmling <arthurwarmling1@gmail.com>
2020-07-16 20:03:10 -0300

    Tips System

--- App.js
+++ App.js
@@ -21,10 +21,51 @@ const HeaderText = styled.Text`
     font-size: 25px;
 `;
 
+const CalcButton = styled.Button`
+    margin-top: 10px;
+`;
+
+const ResultArea = styled.View`
+    margin-top: 30px;
+    background-color: #EEE;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const ResultItemTitle = styled.Text`
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+const ResultItem = styled.Text`
+    font-size: 15px;
+    margin-bottom: 30px;
+`;
+
+const PctArea = styled.View`
+    flex-direction: row;
+    margin: 20px;
+`;
+
+const PctItem = styled.Button``;
 
 export default () => {
 
     const [bill, setBill] = useState('');
+    const [tip, setTip] = useState(0);
+    const [perc, setPerc] = useState(10);
+
+    const calc = () => {
+        let nBill = parseFloat(bill);
+
+        if (nBill) {
+            setTip((perc / 100) * nBill);
+        } else {
+            alert("Digite o valor da conta.");
+        }
+    }
 
     return (
         <Page>
@@ -32,8 +73,28 @@ export default () => {
             <Input
                 placeholder="Quanto deu a conta?"
                 placeholderTextColor="#000"
-                keyboardType="numeric" value={bill} onChangeText={n=>setBill(n)}
+                keyboardType="numeric" value={bill} onChangeText={n => setBill(n)}
             />
+
+            <PctArea>
+                <PctItem title="5%" onPress={() => setPerc(5)} />
+                <PctItem title="10%" onPress={() => setPerc(10)} />
+                <PctItem title="15%" onPress={() => setPerc(15)} />
+                <PctItem title="20%" onPress={() => setPerc(20)} />
+            </PctArea>
+
+
+            <CalcButton title={`Calcular ${perc}%`} onPress={calc} />
+            {tip > 0 &&
+                <ResultArea>
+                    <ResultItemTitle>Valor da Conta:</ResultItemTitle>
+                    <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+                    <ResultItemTitle>Valor da Gorjeta:</ResultItemTitle>
+                    <ResultItem>R$ {tip.toFixed(2)} ({perc}%)</ResultItem>
+                    <ResultItemTitle>Valor Total:</ResultItemTitle>
+                    <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+                </ResultArea>
+            }
         </Page>
     );
 }
